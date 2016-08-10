72e793c4695836c6f7e6208cb4b53203adef70e0
Rob Frawley 2nd <rmf@src.run>
2016-08-10 15:57:42 -0400

    added powerline fonts submodule

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git@github.com:src-run/dot-zsh.git $HOME/.dot-zsh
+git clone https://github.com/powerline/fonts.git $HOME/.dot-zsh/fonts
 rm $HOME/.zshrc
 ln -s $HOME/.dot-zsh/rc.zsh $HOME/.zshrc
